79926ca30000d8a695879d5c49a110ab435d7206
Sven Koitka <sven.koitka@fh-dortmund.de>
2017-02-16 17:03:50 +0100

    Added support for ImageNet, Bottleneck-ResUnits and fixed BatchNorm

--- README.md
+++ README.md
@@ -3,23 +3,35 @@ Caffe Wide-Residual-Network (WRN) Generator
 
 This generator is a reimplementation of Wide Residual Networks (WRN) [[1]](#ref1).
 Full-Pre-Activation Residual Units from [[2]](#ref2) are used with two
-convolutional units of size 3x3 per residual unit. Support for bottleneck
-residual units (3 convolutional layers: 1x1, 3x3, 1x1) will be added in the
-future. Currently the generator is implemented only for CIFAR-10/CIFAR-100, but
-ImageNet will be added soon.
+convolutional units of size 3x3 per residual unit. Bottleneck
+residual units (3 convolutional layers: 1x1, 3x3, 1x1) are available by using `--bottleneck`. 
+Currently the generator is implemented for CIFAR-10/CIFAR-100 (32x32 pixels) and ImageNet 
+(224x224 pixels).
 
 How to use
 ----------
+The generator expects a list of residual unit counts per spatial resolution. For CIFAR-10/CIFAR-100
+there are 3 spatial resolutions, for ImageNet 4 spatial resolutions with residual units. 
+
 __WRN-16-4 for CIFAR-10:__  
-Command: `python generate.py cifar10 16 4`  
+Command: `python generate.py cifar10 2,2,2 4`  
 Output: cifar10_WRN-16-4_train_val.prototxt
 
 __WRN-16-4 with Dropout for CIFAR-100:__  
-Command: `python generate.py cifar100 16 4 --dropout=0.3`  
+Command: `python generate.py cifar100 2,2,2 4 --dropout=0.3`  
 Output: cifar100_WRN-16-4_dropout_train_val.prototxt
 
-For more custumization options check the possible arguments with
+__WRN-37-2 for ImageNet with Bottleneck Residual Units:  
+Command: `python generate.py imagenet 3,4,6,3 2 --bottleneck`  
+Output: imagenet_WRN-37-2_train_val.prototxt
+
+For more customization options check the possible arguments with
 `python generate.py --help`.
+
+Notes
+-----
+* First release only used BatchNormLayer without ScaleLayer
+* ToDo: Generate both train_val and deploy prototxt files
 
 References
 ----------
--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2016 Sven Koitka (sven.koitka@fh-dortmund.de)
+Copyright (c) 2016-2017 Sven Koitka (sven.koitka@fh-dortmund.de)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -28,64 +28,70 @@ import caffe
 from caffe import layers as L
 from caffe import params as P
 
+config = {}
 
-def wide_basic(net, basename, num_input, num_output, stride, dropout):
-    conv_params = [
-        {'kernel_size': 3, 'stride': stride, 'pad': 1},
-        {'kernel_size': 3, 'stride': 1, 'pad': 1}
-    ]
 
-    prev_layer = net.tops[net.tops.keys()[-1]]
-    prev_layer2 = prev_layer
+def wide_basic(net, top, basename, num_input, num_output, stride):
+    if config['bottleneck']:
+        conv_params = [
+            {'kernel_size': 1, 'stride': stride, 'pad': 0, 'num_output': num_output / 4},
+            {'kernel_size': 3, 'stride': 1, 'pad': 1, 'num_output': num_output / 4},
+            {'kernel_size': 1, 'stride': 1, 'pad': 0, 'num_output': num_output}
+        ]
+        dropout_layers = [1]
+    else:
+        conv_params = [
+            {'kernel_size': 3, 'stride': stride, 'pad': 1, 'num_output': num_output},
+            {'kernel_size': 3, 'stride': 1, 'pad': 1, 'num_output': num_output}
+        ]
+        dropout_layers = [1]
+
+    resunit_layer = top
+    shortcut_layer = top
 
     for i, p in enumerate(conv_params):
-        if i == 0:
-            bn = L.BatchNorm(prev_layer)
-            relu = L.ReLU(bn, in_place=True)
-            conv = L.Convolution(relu, num_output=num_output, weight_filler={'type': 'msra'}, bias_term=False, **p)
+        branch_layer_name = '%sa_%d' % (basename, i + 1)
+        add_dropout = i in dropout_layers and config['dropout']
 
-            net[basename + 'a_1_bn'] = bn
-            net[basename + 'a_1_relu'] = relu
-            net[basename + 'a_1_%dx%d_s%d' % (p['kernel_size'], p['kernel_size'], p['stride'])] = conv
-            prev_layer = conv
-        else:
-            bn = L.BatchNorm(prev_layer, in_place=True)
-            relu = L.ReLU(bn, in_place=True)
-            if dropout:
-                drop = L.Dropout(relu, in_place=True, dropout_ratio=dropout)
-            conv = L.Convolution(drop if dropout else relu, num_output=num_output, weight_filler={'type': 'msra'},
-                                 bias_term=False, **p)
+        bn = L.BatchNorm(resunit_layer, in_place=i > 0)#, batch_norm_param={'use_global_stats': True})Bat
+        scale = L.Scale(bn, in_place=True, scale_param={'bias_term': True})
+        relu = L.ReLU(scale, in_place=True)
+        if add_dropout:
+            drop = L.Dropout(relu, in_place=True, dropout_ratio=config['dropout'])
+        conv = L.Convolution(drop if add_dropout else relu, weight_filler={'type': 'msra'}, bias_term=False, **p)
 
-            net[basename + 'a_2_bn'] = bn
-            net[basename + 'a_2_relu'] = relu
-            if dropout:
-                net[basename + 'a_2_dropout'] = drop
-            net[basename + 'a_2_%dx%d_s%d' % (p['kernel_size'], p['kernel_size'], p['stride'])] = conv
-            prev_layer = conv
+        net[branch_layer_name + '_bn'] = bn
+        net[branch_layer_name + '_scale'] = scale
+        net[branch_layer_name + '_relu'] = relu
+        if add_dropout:
+            net[branch_layer_name + '_dropout'] = drop
+        net[branch_layer_name + '_%dx%d_s%d' % (p['kernel_size'], p['kernel_size'], p['stride'])] = conv
+        resunit_layer = conv
 
     if num_input != num_output:
-        conv = L.Convolution(prev_layer2, kernel_size=1, stride=stride, pad=0, num_output=num_output,
+        conv = L.Convolution(shortcut_layer, kernel_size=1, stride=stride, pad=0, num_output=num_output,
                              weight_filler={'type': 'xavier'}, bias_term=False)
         net['%sb_1x1_s%d' % (basename, stride)] = conv
-        prev_layer2 = conv
-    eltwise = L.Eltwise(prev_layer, prev_layer2, operation=P.Eltwise.SUM)
+        shortcut_layer = conv
+    eltwise = L.Eltwise(resunit_layer, shortcut_layer, operation=P.Eltwise.SUM)
     net[basename] = eltwise
+    return eltwise
 
 
-def layer(net, basename, block, num_input, num_output, count, stride, dropout):
-    block(net, basename + '_1', num_input, num_output, stride, dropout)
+def layer(net, top, basename, block, num_input, num_output, count, stride):
+    top = block(net, top, basename + '_1', num_input, num_output, stride)
     for i in range(1, count):
-        block(net, '%s_%d' % (basename, i+1), num_output, num_output, 1, dropout)
+        top = block(net, top, '%s_%d' % (basename, i+1), num_output, num_output, 1)
+    return top
 
 
-def wrn_cifar(depth, k, train_batch_size, test_batch_size, num_classes, db_train, db_test, dropout):
+def wrn_cifar(num_residual_units, k, train_batch_size, test_batch_size, num_classes, db_train, db_test):
     assert k >= 1
-    assert (depth - 4) % 6 == 0
+    assert len(num_residual_units) == 3
 
     n = caffe.NetSpec()
 
     num_stages = [16, 16*k, 32*k, 64*k]
-    num_residual_units = (depth - 4) / 6
 
     n.data, n.label = L.Data(batch_size=train_batch_size, include={'phase': caffe.TRAIN}, backend=P.Data.LMDB,
                              source=db_train, ntop=2,
@@ -107,13 +113,13 @@ def wrn_cifar(depth, k, train_batch_size, test_batch_size, num_classes, db_train
     n.conv1 = L.Convolution(n.data, kernel_size=3, stride=1, pad=1, num_output=num_stages[0],
                             weight_filler={'type': 'msra'}, bias_term=False)
 
-    layer(n, 'res2', wide_basic, num_stages[0], num_stages[1], num_residual_units, 1, dropout)
-    layer(n, 'res3', wide_basic, num_stages[1], num_stages[2], num_residual_units, 2, dropout)
-    layer(n, 'res4', wide_basic, num_stages[2], num_stages[3], num_residual_units, 2, dropout)
+    top = layer(n, n.conv1, 'res2', wide_basic, num_stages[0], num_stages[1], num_residual_units[0], 1)
+    top = layer(n, top, 'res3', wide_basic, num_stages[1], num_stages[2], num_residual_units[1], 2)
+    top = layer(n, top, 'res4', wide_basic, num_stages[2], num_stages[3], num_residual_units[2], 2)
 
-    prev_layer = n.tops[n.tops.keys()[-1]]
-    n.bn_5 = L.BatchNorm(prev_layer)
-    n.relu_5 = L.ReLU(n.bn_5, in_place=True)
+    n.bn_5 = L.BatchNorm(top)#, batch_norm_param={'use_global_stats': True})
+    n.scale_5 = L.Scale(n.bn_5, in_place=True, scale_param={'bias_term': True})
+    n.relu_5 = L.ReLU(n.scale_5, in_place=True)
     n.pool_5 = L.Pooling(n.relu_5, pool=P.Pooling.AVE, global_pooling=True)
     fc = L.InnerProduct(n.pool_5, name='fc%d' % num_classes, param=[dict(lr_mult=1, decay_mult=1),
                                                                     dict(lr_mult=2, decay_mult=0)],
@@ -125,33 +131,97 @@ def wrn_cifar(depth, k, train_batch_size, test_batch_size, num_classes, db_train
     n['score/top-1'] = L.Accuracy(fc, n.label, top_k=1)
     n['score/top-5'] = L.Accuracy(fc, n.label, top_k=5)
 
-    return str(n.to_proto()).replace('__test', '')
+    depth = 1 + len(num_residual_units) + sum([x * 2 for x in num_residual_units])
+    return str(n.to_proto()).replace('__test', ''), depth
 
+
+def wrn_imagenet(num_residual_units, k, train_batch_size, test_batch_size, num_classes, db_train, db_test):
+    assert k >= 1
+    assert len(num_residual_units) == 4
+
+    n = caffe.NetSpec()
+
+    num_stages = [64, 256 * k, 512 * k, 1024 * k, 2048 * k]
+
+    n.data, n.label = L.Data(batch_size=train_batch_size, include={'phase': caffe.TRAIN}, backend=P.Data.LMDB,
+                             source=db_train, ntop=2,
+                             transform_param={
+                                 'mirror': True,
+                                 'crop_size': 224,
+                                 'mean_file': 'mean.binaryproto',
+                                 'scale': 0.00390625
+                             })
+    n.data__test, n.label__test = L.Data(batch_size=test_batch_size, include={'phase': caffe.TEST},
+                                         backend=P.Data.LMDB, source=db_test, ntop=2,
+                                         transform_param={
+                                             'mirror': False,
+                                             'crop_size': 224,
+                                             'mean_file': 'mean.binaryproto',
+                                             'scale': 0.00390625
+                                         })
+
+    n.conv1 = L.Convolution(n.data, kernel_size=7, stride=2, pad=3, num_output=num_stages[0],
+                            weight_filler={'type': 'msra'}, bias_term=False)
+    n.pool1 = L.Pooling(n.conv1, pool=P.Pooling.MAX, stride=2, kernel_size=3)
+
+    top = layer(n, n.pool1, 'res2', wide_basic, num_stages[0], num_stages[1], num_residual_units[0], 1)
+    top = layer(n, top, 'res3', wide_basic, num_stages[1], num_stages[2], num_residual_units[1], 2)
+    top = layer(n, top, 'res4', wide_basic, num_stages[2], num_stages[3], num_residual_units[2], 2)
+    top = layer(n, top, 'res5', wide_basic, num_stages[3], num_stages[4], num_residual_units[3], 2)
+
+    n.bn_6 = L.BatchNorm(top)#, batch_norm_param={'use_global_stats': True})
+    n.scale_6 = L.Scale(n.bn_6, in_place=True, scale_param={'bias_term': True})
+    n.relu_6 = L.ReLU(n.scale_6, in_place=True)
+    n.pool_6 = L.Pooling(n.relu_6, pool=P.Pooling.AVE, global_pooling=True)
+    fc = L.InnerProduct(n.pool_6, name='fc%d' % num_classes, param=[dict(lr_mult=1, decay_mult=1),
+                                                                    dict(lr_mult=2, decay_mult=0)],
+                        num_output=num_classes, weight_filler={'type': 'gaussian', 'std': 0.01},
+                        bias_filler={'type': 'constant', 'value': 0})
+    n[fc.fn.params['name']] = fc
+
+    n['score/loss'] = L.SoftmaxWithLoss(fc, n.label)
+    n['score/top-1'] = L.Accuracy(fc, n.label, top_k=1)
+    n['score/top-5'] = L.Accuracy(fc, n.label, top_k=5)
+
+    depth = 1 + len(num_residual_units) + sum([x * 2 for x in num_residual_units])
+    return str(n.to_proto()).replace('__test', ''), depth
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('dataset', choices=['cifar10', 'cifar100'])
-    parser.add_argument('depth', type=int, help='Depth of the entire network (for CIFAR-10/CIFAR-100: (depth-4)%%6 == 0)')
+    parser.add_argument('dataset', choices=['cifar10', 'cifar100', 'imagenet'])
+    parser.add_argument('num_residual_units', help='Number of residual units per spatial resolution (e.g. "3,3,3" for CIFAR-10/CIFAR-100)')
     parser.add_argument('k', type=int, help='Widening factor for convolutional layers')
     parser.add_argument('--dropout', type=float, help='If specifed then a dropout layer is inserted between convolutional layers in a residual unit')
     parser.add_argument('--batch-size-train', type=int, help='Batch size for the training phase')
     parser.add_argument('--batch-size-test', type=int, help='Batch size for the test phase')
     parser.add_argument('--lmdb-train', help='Path to the training LMDB')
     parser.add_argument('--lmdb-test', help='Path to the test LMDB')
+    parser.add_argument('--bottleneck-resunit', default=False, action='store_true',
+                        help='Uses 1x1, 3x3, 1x1 convolutional layers with reduction of feature maps')
     args = parser.parse_args()
+
+    config['bottleneck'] = args.bottleneck_resunit
+    config['dropout'] = args.dropout
 
     assert args.k >= 1
 
-    prototxt_path = '%s_WRN-%d-%d' % (args.dataset, args.depth, args.k)
-    if args.dropout:
-        prototxt_path += '_dropout'
-    prototxt_path +=  '_train_val.prototxt'
+    num_residual_units = [int(x) for x in args.num_residual_units.split(',')]
 
     if args.dataset in ['cifar10', 'cifar100']:
-        np = wrn_cifar(depth=args.depth, k=args.k, train_batch_size=args.batch_size_train or 128,
+        np, depth = wrn_cifar(num_residual_units=num_residual_units, k=args.k, train_batch_size=args.batch_size_train or 128,
                        test_batch_size=args.batch_size_test or 100, num_classes=10 if args.dataset == 'cifar10' else 100,
-                       db_train=args.lmdb_train or 'db_train_data.lmdb', db_test=args.lmdb_test or 'db_test_data.lmdb',
-                       dropout=args.dropout)
+                       db_train=args.lmdb_train or 'db_train_data.lmdb', db_test=args.lmdb_test or 'db_test_data.lmdb')
+    elif args.dataset == 'imagenet':
+        np, depth = wrn_imagenet(num_residual_units=num_residual_units, k=args.k, train_batch_size=args.batch_size_train or 32,
+                       test_batch_size=args.batch_size_test or 10, num_classes=1000,
+                       db_train=args.lmdb_train or 'db_train_data.lmdb', db_test=args.lmdb_test or 'db_test_data.lmdb')
+
+    prototxt_path = '%s_WRN-%d-%d' % (args.dataset, depth, args.k)
+    if config['bottleneck']:
+        prototxt_path += '_bottleneck'
+    if config['dropout']:
+        prototxt_path += '_dropout'
+    prototxt_path += '_train_val.prototxt'
 
     with open(prototxt_path, 'w') as ofile:
         ofile.write(np)
